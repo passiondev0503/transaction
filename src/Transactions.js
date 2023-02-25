f8fcc3367d96a3291acc624d5b5eabb209daf000
passiondev0503 <passiondev0503@gmail.com>
2023-02-25 16:02:57 +0100

    commit base code

---
+++ src/Transactions.js
@@ -0,0 +1,61 @@
+export default [
+    {
+        id: 1,
+        value: 1000,
+        type: "income",
+        date: "Nov 12 2018 10:00:00 AM",
+    },
+    {
+        id: 2,
+        value: 2000,
+        type: "consumption",
+        date: "Nov 13 2018 10:00:00 AM",
+    },
+    {
+        id: 3,
+        value: 500,
+        type: "income",
+        date: "Mar 12 2012 10:00:00 AM",
+    },
+    {
+        id: 4,
+        value: 2000,
+        type: "consumption",
+        date: "Mar 12 2012 10:00:00 AM",
+    },
+    {
+        id: 5,
+        value: 2000,
+        type: "consumption",
+        date: "Mar 12 2012 10:00:00 AM",
+    },
+    {
+        id: 6,
+        value: 2000,
+        type: "consumption",
+        date: "Mar 12 2012 10:00:00 AM",
+    },
+    {
+        id: 7,
+        value: 2000,
+        type: "consumption",
+        date: "Mar 12 2012 10:00:00 AM",
+    },
+    {
+        id: 8,
+        value: 2000,
+        type: "consumption",
+        date: "Mar 12 2012 10:00:00 AM",
+    },
+    {
+        id: 9,
+        value: 2000,
+        type: "consumption",
+        date: "Mar 12 2012 10:00:00 AM",
+    },
+    {
+        id: 10,
+        value: 2000,
+        type: "consumption",
+        date: "Mar 12 2012 10:00:00 AM",
+    }];
---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function main() {
+  return <h1>hello</h1>;
+}
