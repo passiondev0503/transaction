d7966966db55ab443818e178268350ab0abdd5ca
passiondev0503 <passiondev0503@gmail.com>
2023-02-25 18:01:28 -0500

    modified from javascript to typescript

--- src/layouts/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <>
-      <h1>Transaction Analyzer</h1>
-    </>
-  );
-};
--- src/layouts/index.js
+++
@@ -1,2 +0,0 @@
-export { Content as Body } from './Content';
-export { Header as Title } from './Header';
